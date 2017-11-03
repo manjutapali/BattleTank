97ebf60d5fa2a5811da5f49af5cba6daad896d02
manjutapali <tmanjunath04@gmail.com>
2017-11-03 15:47:35 +0530

    Added AI Controller c++ class, tanks in the world will possess the AI controller

---
+++ Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankAIController.h"
+
+
+void ATankAIController::BeginPlay()
+{
+    Super::BeginPlay();
+    
+    auto ControlledTank = GetControlledTank();
+    
+    if(!ControlledTank)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("No AI controlled Tank"))
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("AI controlled Tank = %s"), *(ControlledTank->GetName()))
+    }
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+    return Cast<ATank>(GetPawn());
+}
+
+
+
